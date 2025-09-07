0796351eee8772d4c7f2561f54cb39e44caa15d2
shakthi <shakthishetty154@gmail.com>
2025-09-07 12:04:02 +0530

    hero Section completed with responsive design which is matching the figma

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,24 +1,29 @@
+
+
+import { LogoSection } from "@/components/HeroSection";
 import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
-    <main className="bg-black min-h-screen flex items-center justify-center px-6 text-white">
-      <div className="text-center max-w-2xl space-y-8">
-       <h1 className="text-4xl md:text-5xl font-bold leading-snug">
-            Future of Advanced<br />
-           <span className="mt-2 block">Materials</span>
-         </h1>
+    <>
+      <main className="bg-black text-white px-6">
+        <div className="max-w-2xl mx-auto text-center space-y-8 pt-16 md:pt-0 md:flex md:flex-col md:items-center md:justify-center min-h-[60vh] md:min-h-[calc(100vh-64px)]">
+          <h1 className="text-4xl md:text-7xl font-bold leading-snug">
+            Future of Advanced <br />
+            <span className="mt-2 block">Materials</span>
+          </h1>
 
+          <p className="text-base text-gray-300">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor
+            incididunt ut labore et.
+          </p>
 
-        <p className="text-base text-gray-300">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor
-          incididunt ut labore et .
-        </p>
+          <Button variant="default">Get Started</Button>
+        </div>
+      </main>
 
-        <Button variant="default">
-          Get Started
-        </Button>
-      </div>
-    </main>
+      <LogoSection />
+    </>
   );
+
 }
